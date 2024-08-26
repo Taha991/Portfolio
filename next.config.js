6daa432a2aba71ddfe66dc83c08af3f2a77e652a
Tahamohamed991 <Tahamohamed991@outlook.com>
2024-08-26 11:56:43 +0300

    e

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/Portfolio",
-  assetPrefix: "/Portfolio/",
-  trailingSlash: true, // ensures that the URLs end with a slash
-  output: "export", // This enables static HTML export with the App Router
-};
-
-module.exports = nextConfig;
+    basePath: "/Portfolio", // Matches your GitHub Pages repo name
+    assetPrefix: "/Portfolio/",
+    trailingSlash: true, // Ensures URLs end with a slash
+    output: 'export', // Enables static HTML export
+  };
+  
+  module.exports = nextConfig;
+  
